32b35b848acb262b9f3581ac3e1709e63688f981
Grace Colette <ilovegrace09@gmail.com>
2019-07-17 19:55:47 +0000

    Automatically backed up by Learn

--- lib/my_all.rb
+++ lib/my_all.rb
@@ -2,7 +2,7 @@ def my_all?(collection)
   i = 0 
   block_return_values = []
   while collection.length > i
-  yield(collection[i])
+  block_return_values << yield(collection[i])
   i += 1
   end
 end
